1291e4f373607abf49a6e424b9398dacc2f333e6
Kevin Van den Abeele <kervin.vandenabeele@gmail.com>
2019-06-30 13:06:16 +0200

    Added readme and fixed dependencies
    Also cleaned up the code a bit

---
+++ readme.md
@@ -0,0 +1,10 @@
+#Yolo V3 Node
+
+This project brings Yolo V3 to node and can be run on a raspberry pi (only tested on 4GB raspberry pi 4).
+
+For local testing just execute `npm install` and then `npm run exec` to start the program.
+Make sure you have a recent version of node installed!
+
+For running on the raspberry pi it is required to first execute: `npm run install-debs-raspberry` before running `npm install`
+
+This project is based on: https://github.com/zqingr/tfjs-yolov3/blob/master/README_EN.md
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import {yolov3, yolov3Tiny} from "./yolo/yolo";
 import {Image, loadImage} from "canvas";
 
 async function start () {
-    //const yolo = await yolov3Tiny();  // pre-load model (35M)
-    const yolo = await yolov3();        // pre-load model (245M)
+    //const yolo = await yolov3Tiny();
+    const yolo = await yolov3();
 
     //const image = await loadImage('./images/example.jpeg');
     setInterval(async () => {
 
         //Try online with: https://zqingr.github.io/tfjs-yolov3-demo/
-        const image: Image = await loadImage('http://192.168.1.8/snap.jpeg');
+        const image: Image = await loadImage('./resources/images/example.jpg');
         const boxes = await yolo(image);
         console.log(boxes);
     }, 2500);
